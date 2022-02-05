999a7350e5c810dd4226af5a06b74db7d19a5e9a
Nimet <76903207+nimelica@users.noreply.github.com>
2022-02-05 14:53:36 -0600

    Update sendNotification.py

--- sendNotification.py
+++ sendNotification.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-sid = 'AC9c86dc17a0164d1ecba9c0cfad2b97bc'
-auth_token = '20eb0ea2090178931b89eb94ab839fb3'
+sid = 'copy and paste account sid from Twilio'
+auth_token = 'add your authentication tokenize number here from Twilio'
 # authenticate our phone
 client = Client(sid, auth_token)
 
@@ -14,12 +14,12 @@ class Notification:
     def verification_message(self):
         message = client.messages.create(to='whatsapp:+Your Phone Number',
                                               from_='whatsapp:+14155238886',
-                                              body='We received your oder!')
+                                              body='We received your order!')
 
     def ready_message(self):
         message = client.messages.create(to='whatsapp:+Your Phone Number',
                                               from_='whatsapp:+14155238886',
-                                              body='Your edited photographs are ready!')
+                                              body='Your Order is Ready!')
 
 
 def main():
